89fe26478f3e59924ff356f467244b2dc0f6cd23
ZoroRoro147 <85626766+ZoroRoro147@users.noreply.github.com>
2021-07-15 12:12:33 +0530

    maven test demo

---
+++ MavenEclipseProject/src/test/java/com/test/project/MavenEclipseProject/Testdemo.java
@@ -0,0 +1,7 @@
+package com.test.project.MavenEclipseProject;
+
+public class Testdemo {
+	@test
+	public void testdemo() {
+	System.out.println("testing");
+}
---
+++ mavendemo/src/test/java/mavendemo/TestDemo.java
@@ -0,0 +1,11 @@
+package mavendemo;
+
+import org.testng.annotations.Test;
+
+public class TestDemo {
+	
+	@Test
+	public void testdemo() {
+		System.out.println("testing maven");
+	}
+}
